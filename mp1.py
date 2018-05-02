61cc773cbc13d0511cddfcb788ed609394bc3eda
Thomas Damiani <thomasdamiani13@gmail.com>
2018-05-02 17:31:18 +0200

    free location ok

--- mp1.py
+++ mp1.py
@@ -144,27 +144,35 @@ def generate_convolutional_network(nb_samples):
     height =100
     width = 100
     depth = 1
-    conv_depth_1 = 100
-    kernel_size = 9
-    pool_size = 2
+    conv_depth_1 = 15
+    kernel_size = 5
+    pool_size = 4
     hidden_size = 512
+    drop_prob_1 = 0.2
+    drop_prob_2 = 0.5
+    nb_epochs = 20
+    batch_size = 32
     inp = Input(shape=(height, width, depth))
+
     conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
     pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
-    flat = Flatten()(pool_1)
+    drop_1 = Dropout(drop_prob_1)(pool_1)
+    flat = Flatten()(drop_1)
     hidden = Dense(hidden_size, activation='relu')(flat)
-    out = Dense(num_classes, activation='softmax')(hidden)
+    drop_2 = Dropout(drop_prob_2)(hidden)
+    out = Dense(num_classes, activation='softmax')(drop_2)
     model = Model(inputs=inp, outputs=out)
     #create training data
     [X_train, Y_train] = generate_dataset_classification(nb_samples, 20,free_location = True)
     Y_train =keras.utils.to_categorical(Y_train,num_classes=3)
     X_train = np.reshape(X_train, (nb_samples,height,width,depth))
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
-              optimizer='adam', # using the Adam optimiser
+              optimizer=sgd, # using the sgd optimizer
               metrics=['accuracy']) # reporting the accuracy
 
     #training
-    model.fit(X_train, Y_train, epochs=20, batch_size=32)
+    model.fit(X_train, Y_train, epochs=nb_epochs, batch_size=batch_size)
 
     return model
 
@@ -298,48 +306,9 @@ def main():
     # model = generate_linear_classifier(1000,True)
     # test_model(model,100,True)
 
-    model = generate_convolutional_network(100)
+    model = generate_convolutional_network(1000)
     test_model2(model,100,True)
 
 
 
 main()
-
-# height, width = 100, 100
-# depth = 1
-# num_classes = 3
-# batch_size = 32 # in each iteration, we consider 32 training examples at once
-# num_epochs = 200 # we iterate 200 times over the entire training set
-# kernel_size = 3 # we will use 3x3 kernels throughout
-# pool_size = 2 # we will use 2x2 pooling throughout
-# conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
-# conv_depth_2 = 64 # ...switching to 64 after the first pooling layer
-# drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
-# drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
-# hidden_size = 512 # the FC layer will have 512 neurons
-# inp = Input(shape=(height, width, depth)) # depth goes last in TensorFlow back-end (first in Theano)
-# # Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
-# conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
-# pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
-# # Conv [64] -> Conv [64] -> Pool (with dropout on the pooling layer)
-# # drop_1 = Dropout(drop_prob_1)(pool_1)
-# # conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
-# # conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(conv_3)
-# # pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
-# # drop_2 = Dropout(drop_prob_1)(pool_2)
-# # Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
-# flat = Flatten()(pool_1)
-# hidden = Dense(hidden_size, activation='relu')(flat)
-# # drop_3 = Dropout(drop_prob_2)(hidden)
-# out = Dense(num_classes, activation='softmax')(hidden)
-#
-# model = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
-#
-# model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
-#               optimizer='adam', # using the Adam optimiser
-#               metrics=['accuracy']) # reporting the accuracy
-# [X_train, Y_train] = generate_dataset_classification(nb_samples, 20, True)
-# X_train = np.array([[[[0] for k in range(100)] for i in range(100)]])
-# model.fit(X_train, Y_train,                # Train the model using the training set...
-#           batch_size=batch_size, epochs=num_epochs,
-#           verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
