0a488bbf6e3fa0422e8fe0f9844277b5ad426487
NICOLAS ZIMMERMANN <zimmermann.nicolas@outlook.fr>
2018-05-02 15:05:37 +0200

    convolutional network

--- mp1.py
+++ mp1.py
@@ -133,6 +133,41 @@ def generate_linear_classifier(nb_samples,free_location=False):
     #print(model.get_weights())
 
     return model
+#
+# from keras.datasets import cifar10
+# (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# height, width, depth = X_train[0].shape
+# print( height, width, depth)
+
+def generate_convolutional_network(nb_samples):
+    num_classes = 3
+    height =100
+    width = 100
+    depth = 1
+    conv_depth_1 = 100
+    kernel_size = 9
+    pool_size = 2
+    hidden_size = 512
+    inp = Input(shape=(height, width, depth))
+    conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
+    pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
+    flat = Flatten()(pool_1)
+    hidden = Dense(hidden_size, activation='relu')(flat)
+    out = Dense(num_classes, activation='softmax')(hidden)
+    model = Model(inputs=inp, outputs=out)
+    #create training data
+    [X_train, Y_train] = generate_dataset_classification(nb_samples, 20,free_location = True)
+    Y_train =keras.utils.to_categorical(Y_train,num_classes=3)
+    X_train = np.reshape(X_train, (nb_samples,height,width,depth))
+    model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+              optimizer='adam', # using the Adam optimiser
+              metrics=['accuracy']) # reporting the accuracy
+
+    #training
+    model.fit(X_train, Y_train, epochs=20, batch_size=32)
+
+    return model
+
 
 
 def prediction(X_test,model,verbose=False):
@@ -153,7 +188,24 @@ def prediction(X_test,model,verbose=False):
             print("Triangle : prob " + str(max_prov))
     return ind_prov
 
-def test_model(model,nb_test=100,free_location=False):
+def prediction2(X_test,model,verbose=False):
+    preds = model.predict(np.array([X_test]))[0]
+    max_prov = -1
+    ind_prov = -1
+    for k in range(len(preds)):
+        if(max_prov < preds[k]):
+            max_prov = preds[k]
+            ind_prov = k
+    if verbose:
+        if ind_prov == 0:
+            print("Rectangle : prob " + str(max_prov))
+        elif ind_prov == 1:
+            print("Disk : prob " + str(max_prov))
+        elif ind_prov == 2:
+            print("Triangle : prob " + str(max_prov))
+    return ind_prov
+
+def test_model(model,nb_test=100,free_location=False):#a recoder en utilisant generate_test_set_classification()
     X_test = [generate_a_rectangle(free_location=free_location) for k in range(nb_test)]
     good_prediction = 0
     for x in X_test:
@@ -167,19 +219,34 @@ def test_model(model,nb_test=100,free_location=False):
             good_prediction += 1
     print("disk : ratio " + str(float(good_prediction)/nb_test))
     good_prediction = 0
-    X_test = [generate_a_triangle(free_location=free_location)[:-1] for k in range(nb_test)]
+    X_test = [generate_a_triangle(free_location=free_location)[0] for k in range(nb_test)]
     for x in X_test:
-        if prediction(x[0],model) == 2:
+        if prediction(x[0],model) == 2:#belek
             good_prediction += 1
     print("triangle : ratio " + str(float(good_prediction)/nb_test))
 
-# generate a linear classifier with a training set size 1000 and figures centered -> work
-# model = generate_linear_classifier(1000)
-# test_model(model,100)
-#
-# # generate a linear classifier with a training set size 1000 and figures not centered -> don't work well
-# model = generate_linear_classifier(1000,True)
-# test_model(model,100,True)
+def test_model2(model,nb_test=100,free_location=False):#a recoder en utilisant generate_test_set_classification()
+    X_test = [generate_a_rectangle(free_location=free_location) for k in range(nb_test)]
+    X_test = np.reshape(X_test, (nb_test,100,100,1))
+    good_prediction = 0
+    for x in X_test:
+        if prediction2(x,model) == 0:
+            good_prediction += 1
+    print("rectangle : ratio " + str(float(good_prediction)/nb_test))
+    good_prediction = 0
+    X_test = [generate_a_disk(free_location=free_location) for k in range(nb_test)]
+    X_test = np.reshape(X_test, (nb_test,100,100,1))
+    for x in X_test:
+        if prediction2(x,model) == 1:
+            good_prediction += 1
+    print("disk : ratio " + str(float(good_prediction)/nb_test))
+    good_prediction = 0
+    X_test = [generate_a_triangle(free_location=free_location)[0] for k in range(nb_test)]
+    X_test = np.reshape(X_test, (nb_test,100,100,1))
+    for x in X_test:
+        if prediction2(x,model) == 2:
+            good_prediction += 1
+    print("triangle : ratio " + str(float(good_prediction)/nb_test))
 
 def generate_dataset_regression(nb_samples, noise=0.0):
     # Getting im_size:
@@ -215,3 +282,64 @@ def generate_test_set_regression():
     [X_test, Y_test] = generate_dataset_regression(300, 20)
     Y_test = np_utils.to_categorical(Y_test, 3)
     return [X_test, Y_test]
+
+
+
+
+
+
+
+def main():
+    # generate a linear classifier with a training set size 1000 and figures centered -> work
+    # model = generate_linear_classifier(1000)
+    # test_model(model,100)
+
+    # # generate a linear classifier with a training set size 1000 and figures not centered -> don't work well
+    # model = generate_linear_classifier(1000,True)
+    # test_model(model,100,True)
+
+    model = generate_convolutional_network(100)
+    test_model2(model,100,True)
+
+
+
+main()
+
+# height, width = 100, 100
+# depth = 1
+# num_classes = 3
+# batch_size = 32 # in each iteration, we consider 32 training examples at once
+# num_epochs = 200 # we iterate 200 times over the entire training set
+# kernel_size = 3 # we will use 3x3 kernels throughout
+# pool_size = 2 # we will use 2x2 pooling throughout
+# conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
+# conv_depth_2 = 64 # ...switching to 64 after the first pooling layer
+# drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
+# drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
+# hidden_size = 512 # the FC layer will have 512 neurons
+# inp = Input(shape=(height, width, depth)) # depth goes last in TensorFlow back-end (first in Theano)
+# # Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
+# conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
+# pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
+# # Conv [64] -> Conv [64] -> Pool (with dropout on the pooling layer)
+# # drop_1 = Dropout(drop_prob_1)(pool_1)
+# # conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
+# # conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(conv_3)
+# # pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
+# # drop_2 = Dropout(drop_prob_1)(pool_2)
+# # Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
+# flat = Flatten()(pool_1)
+# hidden = Dense(hidden_size, activation='relu')(flat)
+# # drop_3 = Dropout(drop_prob_2)(hidden)
+# out = Dense(num_classes, activation='softmax')(hidden)
+#
+# model = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
+#
+# model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+#               optimizer='adam', # using the Adam optimiser
+#               metrics=['accuracy']) # reporting the accuracy
+# [X_train, Y_train] = generate_dataset_classification(nb_samples, 20, True)
+# X_train = np.array([[[[0] for k in range(100)] for i in range(100)]])
+# model.fit(X_train, Y_train,                # Train the model using the training set...
+#           batch_size=batch_size, epochs=num_epochs,
+#           verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
